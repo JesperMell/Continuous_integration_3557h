48bfb0e0853a77c5540cc6dcab77f76b6ccf1d2a
lillalysmasken3 <linuseriklarsson@hotmail.com>
2020-03-13 11:37:26 +0100

    initial test file created

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 
-const char* ssid = "ENTER SSID";
-const char* password = "ENTER PASSWORD";
+const char *ssid = "ENTER SSID";
+const char *password = "ENTER PASSWORD";
 const int pin = 13;
 
 AsyncWebServer server(3000);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   WiFi.begin(ssid, password);
 
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -21,21 +23,21 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   digitalWrite(pin, HIGH);
-  
-  server.on("/turn_on", HTTP_GET, [](AsyncWebServerRequest *request){
+
+  server.on("/turn_on", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Turning on the light");
     Serial.println("Turning on the LED");
     digitalWrite(pin, HIGH);
   });
 
-  server.on("/goodbye", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/goodbye", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Turning off the light");
     Serial.println("Turning off the LED");
     digitalWrite(pin, LOW);
   });
 
-server.begin();
-  pinMode (pin, OUTPUT);
+  server.begin();
+  pinMode(pin, OUTPUT);
 }
 
-void loop() { }
+void loop() {}
