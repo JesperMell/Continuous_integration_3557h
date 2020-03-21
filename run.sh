f5707133b9151063117908388e0d3f3dcabb6217
lillalysmasken3 <linuserklarsson@hotmail.com>
2020-03-21 20:39:59 +0100

    changed to correct location for unity

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir -p build
-gcc -c ./test/test.cpp -I./test -I./test/unity -o ./build/test.o
-gcc ./test/unity/unity.o ./build/test.o -o ./build/test
+gcc -c ./test/test.cpp -I./test -I./build/unity/src/unity -o ./build/test.o
+gcc ./build/unity/src/unity.o ./build/test.o -o ./build/test
 ./build/test
