20c7677b34f72dc1f35046894011ef037f595405
lillalysmasken3 <linuserklarsson@hotmail.com>
2020-03-21 23:05:01 +0100

    moved read, init to module.add functionallity read

--- lib/module/module.cpp
+++ lib/module/module.cpp
@@ -1,12 +1,14 @@
 #include <module.h>
 
-extern const int pin;
+char ssid[MAXSIZE] = {};
+char password[MAXSIZE] = {};
+static boolean state = false;
 
 boolean hello(AsyncWebServer *server)
 {
   server->on("/hello", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Turning on the light");
-    
+
     digitalWrite(pin, HIGH);
   });
   boolean check_hello = digitalRead(pin);
@@ -21,4 +23,53 @@ boolean goodbye(AsyncWebServer *server)
   });
   boolean check_goodbye = !digitalRead(pin);
   return check_goodbye;
+}
+
+void read(char *str)
+{
+  int i = 0;
+  while (i < MAXSIZE)
+  {
+    if (Serial.available())
+    {
+      char tmp = Serial.read();
+      if (isAlphaNumeric(tmp) || tmp == ' ')
+      {
+        str[i] = tmp;
+        Serial.print(tmp);
+        i++;
+      }
+      else if (tmp == '\b')
+      {
+        Serial.print("\t Start over: ");
+
+        i = 0;
+      }
+      else if (tmp == '\n')
+      {
+        break;
+      }
+    }
+  }
+}
+
+bool init(AsyncWebServer *server)
+{
+
+  Serial.print("\nEnter ssid: ");
+  read(ssid);
+
+  Serial.print("\nEnter password: ");
+  read(password);
+
+  WiFi.begin(ssid, password);
+  server->begin();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("\nConnecting to WiFi...");
+    state = false;
+  }
+  state = true;
+  return state;
 }
--- lib/module/module.h
+++ lib/module/module.h
@@ -1,16 +1,30 @@
 #ifndef MODULE_H_
 #define MODULE_H_
+#define MAXSIZE 100
 
 #include <WiFi.h>
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 
-/* **************************************************************** */
+const int pin = 13;
+/* ************************main functions****************************** */
 
 boolean hello(AsyncWebServer *server);
 
 boolean goodbye(AsyncWebServer *server);
 
-/* **************************************************************** */
+void read(char *str);
+bool init(AsyncWebServer *);
+
+/* **********************for testing****************************** */
+
+typedef struct
+{
+    boolean (*module_hello)(AsyncWebServer *);
+    boolean (*module_goodbye)(AsyncWebServer *);
+    void (*module_read)(char *);
+    boolean (*module_init)(AsyncWebServer *);
+
+} module_t;
 
 #endif
